873bd3ced7053ab0fd1dbb86fb45e4d14109876d
Admin <Admin@192.168.0.12>
2020-04-08 12:07:16 +0530

    Commit 1

--- amazon/src/test/java/com/amazon/testcases/Add_To_Cart.java
+++ amazon/src/test/java/com/amazon/testcases/Add_To_Cart.java
@@ -19,13 +19,13 @@ public class Add_To_Cart {
 	static WebDriver driver;
 	public static ExtentTest logger;
 	public static ExtentReports report;
-
+     String s=this.getClass().getSimpleName();
 	@BeforeTest
 	@Parameters({ "browser", "url" })
-	public static void setup(String browser, String url) {
+	public  void setup(String browser, String url) {
 		driver = BrowserFactory.startBrowser(browser, url);
 		report = new ExtentReports(System.getProperty("user.dir") + "\\ExtentReportResults.html");
-		logger = report.startTest("Adding Carts to the items and deleting it");
+		logger = report.startTest(s);
 	}
 
 	@Test
@@ -38,6 +38,7 @@ public class Add_To_Cart {
 	@AfterTest
 	public void tearDownReport() {
 		driver.quit();
+		report.endTest(logger);
 		report.flush();
 	}
 }
